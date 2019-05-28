5caffadbea45815aaa7565838c59120b9390e434
gyeongwon seo <altorius1921@naver.com>
2019-05-29 02:57:58 +0900

    Split compilation
    The 'Init_Game' and 'Initialize_Control_Board' were declared in game_setting.c
    and modified to be defined in game_setting.c
    And the 'Input_Replay_Game' function was declared in print.c
    and modified to be defined in print.c
    
    Resolved: #14

--- game_setting.h
+++ game_setting.h
@@ -1,5 +1,7 @@
 //The game board can change by changing this defined constant
 #define BOARD_SIZE 6
 
-int Initialize_Control_Board(char control_board[BOARD_SIZE][BOARD_SIZE], int difficulty);
+void Init_Game(char control_board[BOARD_SIZE][BOARD_SIZE], char showed_board[BOARD_SIZE][BOARD_SIZE]);
 int Select_Difficulty();
+int Initialize_Control_Board(char control_board[BOARD_SIZE][BOARD_SIZE], int difficulty);
+void Initialize_Showed_Board(char showed_board[BOARD_SIZE][BOARD_SIZE]);
