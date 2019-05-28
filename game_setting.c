1e3d0b4e5162a33d670efd149611dfa5fa0bf655
gyeongwon seo <altorius1921@naver.com>
2019-05-29 04:06:32 +0900

    Split compilation
    The 'Init_Game' and 'Initialize_Showed_Board' were declared in game_setting.c
    and modified to be defined in game_setting.c
    And the 'Input_Replay_Game' function was declared in print.c
    and modified to be defined in print.c
    
    Resolved: #14

--- game_setting.c
+++ game_setting.c
@@ -7,46 +7,23 @@
 //The game board can change by changing this defined constant
 #define BOARD_SIZE 6
 
-int Initialize_Control_Board(char control_board[BOARD_SIZE][BOARD_SIZE], int difficulty) {
-    /*
-    Uses the difficulty parameter to generate bombs on the board.
-    This board won't be showed to the user until the game ends.
-    Empty spaces are 'o', bombs are '*'. The function returns the number of bombs on the board
-    */
+void Init_Game(char control_board[BOARD_SIZE][BOARD_SIZE], char showed_board[BOARD_SIZE][BOARD_SIZE]) {
+	/*
+	 *@brief   게임의 난이도, 폭탄 수, 게임판 등을 초기화 하는 함수
+	 *@param   control_board[][] : 사용자에게 보이지 않는 게임판
+	 *		   showed_board[][] : 사용자에게 보이는 게임판
+	 */
+	
+	int difficulty;
+	int number_of_bombs;
 
-    //Using time as seed
-    time_t t;
-    srand((unsigned int) (&t));
+	difficulty = Select_Difficulty();
 
-    int counter = 0;
-    int random_number;
-	int row, col;
+    number_of_bombs = Initialize_Control_Board(control_board, difficulty);
+    printf("\nThe board has %d bombs. Here we go again!\n", number_of_bombs);
 
-    for (row = 0; row < BOARD_SIZE; row++) {
-
-        for(col = 0; col < BOARD_SIZE; col++) {
-
-            //Random number range goes from 0 to 10
-            random_number = rand() % 11;
-
-            //Comparing the random number and difficulty parameter. Note that the chance of generating a bomb
-            //becomes higher as the difficulty parameter becomes higher (it is set as 1, 3 or 6 now)
-            if (random_number < difficulty) {
-
-                control_board[row][col] = '*';
-                counter ++;
-            }
-
-            else {
-
-                control_board[row][col] = 'o';
-            }
-        }
-    }
-
-    return counter;
+    Initialize_Showed_Board(showed_board);
 }
-
 
 
 int Select_Difficulty() {
@@ -92,4 +69,62 @@ int Select_Difficulty() {
     } 
 
     return difficulty;
+}
+
+
+int Initialize_Control_Board(char control_board[BOARD_SIZE][BOARD_SIZE], int difficulty) {
+    /*
+    Uses the difficulty parameter to generate bombs on the board.
+    This board won't be showed to the user until the game ends.
+    Empty spaces are 'o', bombs are '*'. The function returns the number of bombs on the board
+    */
+
+    //Using time as seed
+    time_t t;
+    srand((unsigned int) (&t));
+
+    int counter = 0;
+    int random_number;
+	int row, col;
+
+    for (row = 0; row < BOARD_SIZE; row++) {
+
+        for(col = 0; col < BOARD_SIZE; col++) {
+
+            //Random number range goes from 0 to 10
+            random_number = rand() % 11;
+
+            //Comparing the random number and difficulty parameter. Note that the chance of generating a bomb
+            //becomes higher as the difficulty parameter becomes higher (it is set as 1, 3 or 6 now)
+            if (random_number < difficulty) {
+
+                control_board[row][col] = '*';
+                counter ++;
+            }
+
+            else {
+
+                control_board[row][col] = 'o';
+            }
+        }
+    }
+
+    return counter;
+}
+
+
+void Initialize_Showed_Board(char showed_board[BOARD_SIZE][BOARD_SIZE]) {
+    /*
+    Assigns 'X' for the whole showed_board
+    */
+	/*
+	 *@brief   showed_board[][]를 모두 'X'로 초기화 ('X'는 지뢰를 검사하기 전 상태)
+	 *@param   showed_board[][] : 사용자가 볼 수 있는 게임판
+	 */
+
+    for (int row = 0; row < BOARD_SIZE; row++) {
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            showed_board[row][col] = 'X';
+        }
+    }
 }
