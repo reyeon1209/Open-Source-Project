fb1191dc6e9dbafdb2735c6a0ace3db116d09637
reyeon1209 <reyeon5368@naver.com>
2019-05-31 12:51:40 +0900

    Change bomb placement timing
    In order to prevent the gameover from happening after the start, the
     user picked the first position and then modified to place the bomb.
    
    resolved : #36

--- main.c
+++ main.c
@@ -1,99 +1,113 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ctype.h>
 #include <Windows.h>
 #include "print.h"
 #include "game_setting.h"
 
 
-#define BOARD_SIZE 6
 #define TRUE 1
 #define FALSE 0
 // @brief 게임 상태를 나타내는 상수이다. 상수의 값에는 의미가 없다.
-#define INIT (-10)	
+#define INIT (-10)   
 #define WIN (-20)
 #define LOSE (-30)
 #define KEEP_ON (-40)
 #define GAME_OVER (-50)
 
 
-typedef struct Point {
-	int row;
-	int col;
-}Point;
-
-
 Point Get_Board_Position();
-int CheckInput(char row[100], char col[100]);
-int IsOverLimit(int row, int col);
-int Board_Update(char control_board[BOARD_SIZE][BOARD_SIZE], char showed_board[BOARD_SIZE][BOARD_SIZE], Point pos);
-char IntToChar(int n);
-int IsMine(char control_board[BOARD_SIZE][BOARD_SIZE], int row, int col);
+int Check_Input(char row[100], char col[100]);
+int Is_Over_Limit(int row, int col);
+int Update_Board(char control_board[BOARD_SIZE][BOARD_SIZE], char showed_board[BOARD_SIZE][BOARD_SIZE], Point pos);
+char Int_To_Char(int n);
+int Is_Mine(char control_board[BOARD_SIZE][BOARD_SIZE], int row, int col);
 int Get_Around_Mine_Number(char control_board[BOARD_SIZE][BOARD_SIZE], Point pos);
 int Get_Game_Status(char control_board[BOARD_SIZE][BOARD_SIZE], int game_status);
-void GoToXY(Point pos);
+void Remove_Input(Point row_pos, Point col_pos, Point over_pos);
+
 
 int main() {
 
 	char control_board[BOARD_SIZE][BOARD_SIZE];
-    char showed_board[BOARD_SIZE][BOARD_SIZE];
+	char showed_board[BOARD_SIZE][BOARD_SIZE];
 
-    int game_status = INIT;
-	Point pos;
+	int game_status = INIT;
+	int number_of_bombs;
+	int difficulty;
+	Point pos, opos;
 
-    Display_Welcome_Message();
+	const int OVER_MESSAGE_X = 0;
+	const int OVER_MESSAGE_Y = 18;
 
-    while (game_status == INIT || game_status == KEEP_ON) {
+
+	Display_Welcome_Message();
+
+	while (game_status == INIT || game_status == KEEP_ON) {
 		if (game_status == INIT) {
-            Init_Game(control_board, showed_board);
-        }
+			difficulty = Init_Game(control_board, showed_board);
+		}
 
-		Print_Board(showed_board);
-		
+		if (game_status == INIT) {
+			number_of_bombs = Initialize_Control_Board(control_board, difficulty);
+			game_status = KEEP_ON;
+		}
+
 		pos = Get_Board_Position();
-		game_status = Board_Update(control_board, showed_board, pos);
+		game_status = Update_Board(control_board, showed_board, pos);
 		game_status = Get_Game_Status(control_board, game_status);
-    }
 
-    return 0;
+		opos.col = OVER_MESSAGE_X;
+		opos.row = OVER_MESSAGE_Y;
+
+		GoToXY(opos);
+		printf("\nThe board has %d bombs. Here we go again!\n", number_of_bombs);
+	
+	}
+
+	return 0;
 }
 
-
 Point Get_Board_Position() {
-	
+
 	Point pos;
 	Point row_pos, col_pos, over_pos;
-	
+
 	int check_input = FALSE;
 	char row[100], col[100];
 
-	const int INPUT_ROW_X = 14;
-	const int INPUT_ROW_Y = 13;
-	const int INPUT_COL_X = 18;
-	const int INPUT_COL_Y = 14;
-	const int OVER_MESSAGE_X = 0;
-	const int OVER_MESSAGE_Y = 17;
+	const int INPUT_ROW_LEFT = 14;
+	const int INPUT_ROW_TOP = 12;
+	const int INPUT_COL_LEFT = 18;
+	const int INPUT_COL_TOP = 13;
+	const int OVER_MESSAGE_LEFT = 0;
+	const int OVER_MESSAGE_TOP = 16;
 
-	row_pos.col = INPUT_ROW_X;
-	row_pos.row = INPUT_ROW_Y;
-	col_pos.col = INPUT_COL_X;
-	col_pos.row = INPUT_COL_Y;
-	over_pos.col = OVER_MESSAGE_X;
-	over_pos.row = OVER_MESSAGE_Y;
+	row_pos.col = INPUT_ROW_LEFT;
+	row_pos.row = INPUT_ROW_TOP;
+	col_pos.col = INPUT_COL_LEFT;
+	col_pos.row = INPUT_COL_TOP;
+	over_pos.col = OVER_MESSAGE_LEFT;
+	over_pos.row = OVER_MESSAGE_TOP;
 
-    while (!check_input) {
-        scanf(" %s", &row);
-
-        scanf(" %s", &col);		
+	while (!check_input) {
+		row[0] = '\0', col[0] = '\0';
+		GoToXY(row_pos);
+		scanf(" %s", &row);
+		GoToXY(col_pos);
+		scanf(" %s", &col);      
 
 		Remove_Input(row_pos,col_pos,over_pos);
+		check_input = Check_Input(row, col);
 
-		check_input = CheckInput(row, col);
+		if (!check_input) {
+			printf("Should go from 0 to %d. Try again\n", BOARD_SIZE-1);
+		}
 	}
 
-	pos.row = atoi(row);	// int로 변환
+	pos.row = atoi(row);
 	pos.col = atoi(col);
 
 	printf("(r%d,c%d) is opened.                                   ",pos.row, pos.col);
@@ -101,92 +115,102 @@ Point Get_Board_Position() {
 	return pos;
 }
 
-int CheckInput(char row[100], char col[100]) {
+int Check_Input(char row[100], char col[100]) {
 	int num = BOARD_SIZE;
 	int len = 0;
+	int i;
 
-	while (num > 0)	// 보드판이 몇자리수인지
+	while (num > 0)
 	{
 		num = num/10;
 		len++;
 	}
 
-    if (strlen(row) >= len || strlen(col) >= len)	// 보드판크기보다 문자열길이가 길면 다시 입력
-	{
-		printf("Should go from 0 to %d. Try again\n", BOARD_SIZE-1);
+	if (strlen(row) > len || strlen(col) > len)
 		return FALSE;
+
+	for (i = 0; i < strlen(row); i++)
+	{
+		if (isdigit(row[i]));
+		else return FALSE;
 	}
 
-	if (!IsOverLimit(atoi(row), atoi(col)))   // 게임판 크기 내에 있는지
+	for (i = 0; i < strlen(col); i++)
+	{
+		if (isdigit(col[i]));
+		else return FALSE;
+	}
+
+	if (!Is_Over_Limit(atoi(row), atoi(col)))
 		return TRUE;
 
 	return FALSE;
 }
 
-int IsOverLimit(int row, int col) {
+int Is_Over_Limit(int row, int col) {
 
-	if (row >= BOARD_SIZE)	return TRUE;
-	if (col >= BOARD_SIZE)	return TRUE;
-	else	return FALSE;
+	if (row >= BOARD_SIZE)   return TRUE;
+	if (col >= BOARD_SIZE)   return TRUE;
+	else   return FALSE;
 }
 
-int Board_Update(char control_board[BOARD_SIZE][BOARD_SIZE], char showed_board[BOARD_SIZE][BOARD_SIZE], Point pos) {
-   
-    int mine_checker_feedback = Get_Around_Mine_Number(control_board, pos);
+int Update_Board(char control_board[BOARD_SIZE][BOARD_SIZE], char showed_board[BOARD_SIZE][BOARD_SIZE], Point pos) {
+
+	int mine_checker_feedback = Get_Around_Mine_Number(control_board, pos);
 	Point cursor;
 
-    const char OPENED = 'x';
+	const char OPENED = 'x';
 	const int TOP_OF_BOARD = 5;
 	const int LEFT_OF_BOARD = 1;
 	const int NUM_BLANK = 3;
 
-    if (mine_checker_feedback == LOSE) {
+	if (mine_checker_feedback == LOSE) {
 
-        return LOSE;
-    }
+		return LOSE;
+	}
 
-    else {
+	else {
 
-        control_board[pos.row][pos.col] = OPENED;
-        
-        showed_board[pos.row][pos.col] = IntToChar(mine_checker_feedback);
+		control_board[pos.row][pos.col] = OPENED;
+
+		showed_board[pos.row][pos.col] = Int_To_Char(mine_checker_feedback);
 
 		cursor.col = pos.col * NUM_BLANK + LEFT_OF_BOARD;
 		cursor.row = pos.row + TOP_OF_BOARD;
 
 		GoToXY(cursor);
-        printf("%c", showed_board[pos.row][pos.col]);
+		printf("%c", showed_board[pos.row][pos.col]);
 
-        return KEEP_ON;
-    }
+		return KEEP_ON;
+	}
 }
 
-char IntToChar(int n) {
+char Int_To_Char(int n) {
 
 	const int ASCII = 48;
 
 	return n + ASCII;
 }
 
-int IsMine(char control_board[BOARD_SIZE][BOARD_SIZE], int row, int col) {
+int Is_Mine(char control_board[BOARD_SIZE][BOARD_SIZE], int row, int col) {
 	char const MINE = '*';
-	
-	if (control_board[row][col] == MINE)	
+
+	if (control_board[row][col] == MINE)   
 		return TRUE;
-	else	
+	else   
 		return FALSE;
 }
 
 int Get_Around_Mine_Number(char control_board[BOARD_SIZE][BOARD_SIZE], Point pos) {
 	/*
-	 * @brief   주위 폭탄 개수 리턴시, 사용자가 선택한 위치가 보드의 가장자리라면 내부 칸의 폭탄만 검사한다
-	 * @params	control_board : 지뢰가 저장된 보드, row : 사용자가 선택한 보드 행, col : 사용자가 선택한 보드 열
-	 * @return	사용자가 선택한 보드 위치에 지뢰가 있다면 -1 리턴, 그렇지 않다면 주변 폭탄 개수 리턴
-	 */
+	* @brief   주위 폭탄 개수 리턴시, 사용자가 선택한 위치가 보드의 가장자리라면 내부 칸의 폭탄만 검사한다
+	* @params   control_board : 지뢰가 저장된 보드, row : 사용자가 선택한 보드 행, col : 사용자가 선택한 보드 열
+	* @return   사용자가 선택한 보드 위치에 지뢰가 있다면 -1 리턴, 그렇지 않다면 주변 폭탄 개수 리턴
+	*/
 	const char MINE = '*';
 	const int BOARD_START_POINT = 0;
 
-    int number_of_bomb = 0;
+	int number_of_bomb = 0;
 	int start_row_index = -1;
 	int end_row_index = 1;
 	int start_col_index = -1;
@@ -194,11 +218,11 @@ int Get_Around_Mine_Number(char control_board[BOARD_SIZE][BOARD_SIZE], Point pos
 	int row_index;
 	int col_index;
 
-    if (control_board[pos.row][pos.col] == MINE) {
-        return LOSE;
-    }
+	if (control_board[pos.row][pos.col] == MINE) {
+		return LOSE;
+	}
 
-    else {
+	else {
 		if( pos.row == BOARD_START_POINT ) start_row_index = 0;
 		if( pos.col == BOARD_START_POINT ) start_col_index = 0;
 		if( pos.row == BOARD_SIZE - 1) end_row_index = 0;
@@ -206,33 +230,33 @@ int Get_Around_Mine_Number(char control_board[BOARD_SIZE][BOARD_SIZE], Point pos
 
 		for(row_index = start_row_index; row_index <= end_row_index; row_index++) {
 			for(col_index = start_col_index; col_index <= end_col_index; col_index++) {
-				if (IsMine(control_board, pos.row+row_index, pos.col+col_index ))
+				if (Is_Mine(control_board, pos.row+row_index, pos.col+col_index ))
 					number_of_bomb ++;
 			}
 		}
-    }
+	}
 
-    return number_of_bomb;
+	return number_of_bomb;
 }
 
 int Get_Game_Status(char control_board[BOARD_SIZE][BOARD_SIZE], int game_status) {
-    /*
-	 * @brief	현재 게임 상태에 따라서 다음 게임 상태를 리턴하는 함수
-	 * @param   control_board[][] : 사용자에게 보이지 않는 게임판
-	 *			game_status : 게임의 상태
-	 *			next_status : 다음 게임 상태를 나타내는 변수, 
-	 *		                 현재 게임 상태가 WIN이나 LOSE가 아닐 경우 게임을 계속 진행해야 하기 때문에 KEEP_ON으로 초기화
-	 * @return	게임 상태가 WIN이나 LOSE일 경우 Input_Replay_Game()에서 받은 리턴 값(INIT, GAMEOVER),
-	 *			게임 상태가 WIN이나 LOSE가 아닐 경우 KEEP_ON
-	 *			(INIT, GAMEOVER, KEEP_ON은 game_status가 가질 상수)
-	 */
+	/*
+	* @brief   현재 게임 상태에 따라서 다음 게임 상태를 리턴하는 함수
+	* @param   control_board[][] : 사용자에게 보이지 않는 게임판
+	*         game_status : 게임의 상태
+	*         next_status : 다음 게임 상태를 나타내는 변수, 
+	*                       현재 게임 상태가 WIN이나 LOSE가 아닐 경우 게임을 계속 진행해야 하기 때문에 KEEP_ON으로 초기화
+	* @return   게임 상태가 WIN이나 LOSE일 경우 Input_Replay_Game()에서 받은 리턴 값(INIT, GAMEOVER),
+	*         게임 상태가 WIN이나 LOSE가 아닐 경우 KEEP_ON
+	*         (INIT, GAMEOVER, KEEP_ON은 game_status가 가질 상수)
+	*/
 
 	int next_status = KEEP_ON;
 
 	if (game_status == WIN) {
 		system("cls");
 
-        printf("\n\nYou did it! You cleared the board. Congratulations!!!\n\n");
+		printf("\n\nYou did it! You cleared the board. Congratulations!!!\n\n");
 		next_status = Input_Replay_Game(control_board);
 	}
 
@@ -249,19 +273,10 @@ int Get_Game_Status(char control_board[BOARD_SIZE][BOARD_SIZE], int game_status)
 	return next_status;
 }
 
-void GoToXY(Point pos){
-   COORD cursor;
-
-   cursor.X = pos.col;
-   cursor.Y = pos.row;
-
-   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursor);
-}
-
 void Remove_Input(Point row_pos, Point col_pos, Point over_pos) {
 	GoToXY(row_pos);
-	printf("\t\t\t\t\t ");
+	printf("                    ");
 	GoToXY(col_pos);
-	printf("\t\t\t\t\t ");
+	printf("                    ");
 	GoToXY(over_pos);
 }
--- print.c
+++ print.c
@@ -7,15 +7,15 @@
 void Display_Welcome_Message() {
 
 	printf("\n           _                                                   "
-           "\n          (_)                                                  \n"
-           " _ __ ___  _ _ __   ___  _____      _____  ___ _ __   ___ _ __ \n"
-           "| '_ ` _ \\| | '_ \\ / _ \\/ __\\ \\ /\\ / / _ \\/ _ \\ '_ \\ / _ \\ '__|\n"
-           "| | | | | | | | | |  __/\\__ \\\\ V  V /  __/  __/ |_) |  __/ |   \n"
-           "|_| |_| |_|_|_| |_|\\___||___/ \\_/\\_/ \\___|\\___| .__/ \\___|_|   \n"
-           "                                              | |              \n"
-           "                                              |_|              \n\n");
+		"\n          (_)                                                  \n"
+		" _ __ ___  _ _ __   ___  _____      _____  ___ _ __   ___ _ __ \n"
+		"| '_ ` _ \\| | '_ \\ / _ \\/ __\\ \\ /\\ / / _ \\/ _ \\ '_ \\ / _ \\ '__|\n"
+		"| | | | | | | | | |  __/\\__ \\\\ V  V /  __/  __/ |_) |  __/ |   \n"
+		"|_| |_| |_|_|_| |_|\\___||___/ \\_/\\_/ \\___|\\___| .__/ \\___|_|   \n"
+		"                                              | |              \n"
+		"                                              |_|              \n\n");
 
-    printf("Welcome to Minesweeper. Fork me at: https://github.com/apieceofCAKE/minesweeper_game\n");
+	printf("Welcome to Minesweeper. Fork me at: https://github.com/apieceofCAKE/minesweeper_game\n");
 	printf("\n\nLoading...please wait");
 	Sleep(2000);
 }
@@ -25,58 +25,86 @@ void Print_Board(char board[BOARD_SIZE][BOARD_SIZE]) {
 	int col;
 	int row;
 
-    printf("\n");
+	printf("\n");
 
-    for (col = 0; col < BOARD_SIZE; col++) {
-        printf("c%d ", col);
-    }
+	for (col = 0; col < BOARD_SIZE; col++) {
+		printf("c%d ", col);
+	}
 
-    printf("\n\n");
+	printf("\n\n");
 
-    for (row = 0; row < BOARD_SIZE; row++) {
-        for (col = 0; col < BOARD_SIZE; col++) {
-            printf(" %c ", board[row][col]);
+	for (row = 0; row < BOARD_SIZE; row++) {
+		for (col = 0; col < BOARD_SIZE; col++) {
+			printf(" %c ", board[row][col]);
 		}
-        printf("     r%d\n", row);
-    }
+		printf("     r%d\n", row);
+	}
 
 }
 
 int Input_Replay_Game(char control_board[BOARD_SIZE][BOARD_SIZE]) {
 	/*
-	 * @brief	사용자에게 게임을 다시 할지 입력받는 함수
-	 * @param	control_board[][] : 사용자에게 보이지 않는 게임판
-	 *			input : 게임을 다시할 지 사용자의 입력을 받는 변수
-	 * @return	사용자의 입력이 y일 경우 INIT, 사용자의 입력이 n일 경우 GAMEOVER
-	 *			(INIT, GAMEOVER는 game_status가 가질 상수)
-	 */
-	
+	* @brief	사용자에게 게임을 다시 할지 입력받는 함수
+	* @param	control_board[][] : 사용자에게 보이지 않는 게임판
+	*			input : 게임을 다시할 지 사용자의 입력을 받는 변수
+	* @return	사용자의 입력이 y일 경우 INIT, 사용자의 입력이 n일 경우 GAMEOVER
+	*			(INIT, GAMEOVER는 game_status가 가질 상수)
+	*/
+
+
+	char input = '\0';	
+	Point input_pos;
+	Point wrong_pos;
+
 	const char YES = 'y';
 	const char NO = 'n';
-	char input = '\0';	
+	const int INPUT_LEFT = 33;
+	const int INPUT_TOP = 14;
+	const int WRONG_LEFT = 0;
+	const int WRONG_TOP = 17;
+
+	input_pos.col = INPUT_LEFT;
+	input_pos.row =	INPUT_TOP;
+	wrong_pos.col = WRONG_LEFT;
+	wrong_pos.row = WRONG_TOP;
 
 	Print_Board(control_board);
-	
-	while (input != YES && input != NO) {
-		printf("\nDo you want to play again (y/n)? ");
-        scanf(" %c", &input);
-        printf("\n\n");
 
-        if (input == YES) {
+	printf("\nDo you want to play again (y/n)? ");
+
+	while (input != YES && input != NO) {
+		GoToXY(input_pos);
+		scanf(" %c", &input);
+
+		GoToXY(wrong_pos);
+
+		if (input == YES) {
 
 			return INIT;
-        }
+		}
 
-        else if (input == NO) {
-            printf("Thanks for playing!\n\n");
+		else if (input == NO) {
+			printf("Thanks for playing!\n\n");
 
-            return GAME_OVER;
-        }
+			return GAME_OVER;
+		}
 
-        else {
-            printf("Wrong input. Try again...");
-        }
+		else {
+			printf("Wrong input. Try again...");
+		}
+
+		GoToXY(input_pos);
+		printf("                    ");
 	}
 
 	return GAME_OVER;
+}
+
+void Go_To_XY(Point pos){
+	COORD cursor;
+
+	cursor.X = pos.col;
+	cursor.Y = pos.row;
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursor);
 }
